743f69673c635e8ee4fd053c4a1287f1f4d76592
Chron1um635 <chron1um522@gmail.com>
2024-10-08 19:58:42 +0300

    Initial Commit

---
+++ ColorizedApp/ColorizedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedAppApp.swift
+//  ColorizedApp
+//
+//  Created by Максим Назаров on 08.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
